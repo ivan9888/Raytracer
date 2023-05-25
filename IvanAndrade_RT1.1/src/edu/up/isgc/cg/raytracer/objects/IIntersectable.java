ec57ade16364ea0e83e7005a346af834fa27710c
ivan9888 <92829634+ivan9888@users.noreply.github.com>
2023-05-24 20:05:34 -0600

    Add files via upload

---
+++ IvanAndrade_RT1.1/src/edu/up/isgc/cg/raytracer/objects/IIntersectable.java
@@ -0,0 +1,15 @@
+/**
+ * [1968] - [2023] Centros Culturales de Mexico A.C / Universidad Panamericana
+ * All Rights Reserved.
+ */
+package edu.up.isgc.cg.raytracer.objects;
+
+import edu.up.isgc.cg.raytracer.Intersection;
+import edu.up.isgc.cg.raytracer.Ray;
+
+/**
+ * @author Jafet Rodríguez
+ */
+public interface IIntersectable {
+    Intersection getIntersection(Ray ray);
+}
